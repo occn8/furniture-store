223c558c7a485d9e955d252134e4404a13c97f95
klivekaru08 <klive-karu@outlook.com>
2020-12-18 16:35:47 +0300

    contact page

---
+++ site/contact.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fur-Store Contact</title>
+</head>
+
+<body>
+    <?php echo '<div class="dummytest">
+        <h1>Fur-Store Contact</h1>
+    </div>'; ?>
+</body>
+
+</html>

eeb0635eda4260964e3b7825e8e9e47d4b25ba9e
klivekaru08 <klive-karu@outlook.com>
2020-12-18 16:32:16 +0300

    about page

--- site/about.php
+++ site/about.php
@@ -4,14 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fur-Store Home</title>
+    <title>Fur-Store AboutUs</title>
 </head>
 
 <body>
     <?php echo '<div class="dummytest">
-        <h1>Fur-Store Home</h1>
+        <h1>Fur-Store AboutUs</h1>
     </div>'; ?>
-
 </body>
 
 </html>

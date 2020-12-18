4dbd4cb6b4386b30fb31338ef16746b5f26af3a5
klivekaru08 <klive-karu@outlook.com>
2020-12-18 16:44:04 +0300

    colections

---
+++ site/collection.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fur-Store Collections</title>
+</head>
+
+<body>
+    <?php echo '<div class="dummytest">
+        <h1>Fur-Store collections</h1>
+    </div>'; ?>
+
+</body>
+
+</html>

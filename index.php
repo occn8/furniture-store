ed9091db0f85883dbf05dca457d849e1d0fa9a96
occn8 <ochenwayward@gmail.com>
2020-12-23 16:22:02 +0300

    workforday

--- index.php
+++ index.php
@@ -164,22 +164,31 @@
             <h6 class="text-uppercase mb-1">Categories</h6>
             <ul class="list-unstyled components mb-3">
                 <li>
-                    <a href="#">Category 1</a>
+                    <a href="#">Armchairs</a>
                 </li>
                 <li>
-                    <a href="#">Category 1</a>
+                    <a href="#">Chaise longues</a>
                 </li>
                 <li>
-                    <a href="#">Category 1</a>
+                    <a href="#">Cushions</a>
                 </li>
                 <li>
-                    <a href="#">Category 1</a>
+                    <a href="#">Daybeds</a>
                 </li>
                 <li>
-                    <a href="#">Category 1</a>
+                    <a href="#">Easychairs</a>
                 </li>
                 <li>
-                    <a href="#">Category 1</a>
+                    <a href="#">Footstools</a>
+                </li>
+                <li>
+                    <a href="#">Poufs</a>
+                </li>
+                <li>
+                    <a href="#">Smallsofas</a>
+                </li>
+                <li>
+                    <a href="#">Sofa</a>
                 </li>
             </ul>
 
@@ -192,12 +201,66 @@
 
         </nav>
     </header>
-    <?php echo '<div class="dummytest">
+
+    <main>
+        <?php echo '<div class="dummytest">
         <h1>Fur-Store Home</h1>
     </div>'; ?>
+        <section></section>
+        <section></section>
+        <section></section>
+        <section></section>
+        <section></section>
+    </main>
 
-<script src="assets/scripts/jquery.min.js"></script>
-<script src="assets/scripts/sidebar.js"></script>
+    <footer class="pt-4 text-center my-md-5 pt-md-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-6 col-md-4">
+                    <h5>QUICK LINKS</h5>
+                    <ul class="list-unstyled text-small">
+
+                        <li><a class="text-muted" href="#">Sell online</a></li>
+                        <li><a class="text-muted" href="#">Features</a></li>
+                        <li><a class="text-muted" href="#">Shopping cart</a></li>
+                        <li><a class="text-muted" href="#">Mobile commerce</a></li>
+                        <li><a class="text-muted" href="#">Dropshipping</a></li>
+                        <li><a class="text-muted" href="#">Development</a></li>
+                    </ul>
+                </div>
+                <div class="col-6 col-md-4">
+                    <h5>CONTACT INFO</h5>
+                    <ul class="list-unstyled text-small">
+                        <li><a class="text-muted" href="#">Ntinda valley, 21st st, Kampala, UGANDA</a></li>
+                        <li><a class="text-muted" href="#">+256 7887 06532</a></li>
+                        <li><a class="text-muted" href="#">elitefurniture@furstore.com</a></li>
+                        <li><a class="text-muted" href="#">SUBSCRIBE</a></li>
+                    </ul>
+                </div>
+                <div class="col-6 col-md-4">
+                    <h5>About</h5>
+                    <ul class="list-unstyled text-small">
+                        <li><a class="text-muted" href="#">Team</a></li>
+                        <li><a class="text-muted" href="#">Locations</a></li>
+                        <li><a class="text-muted" href="#">Privacy</a></li>
+                        <li><a class="text-muted" href="#">Terms</a></li>
+                    </ul>
+                </div>
+            </div>
+
+
+            <p class="text-center">
+                Copyright ©<script>
+                    document.write(new Date().getFullYear());
+                </script> All rights reserved | <a href="#" target="_app">Fur-Store</a>
+            </p>
+
+        </div>
+    </footer>
+
+    <script src="assets/scripts/jquery.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/scripts/sidebar.js"></script>
 </body>
 
 </html>

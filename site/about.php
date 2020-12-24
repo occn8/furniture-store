65b920c39a7adddb3685a7236f5d699c02de9e1f
tishakirabo <muhanguzitisha@gmail.com>
2020-12-24 11:15:40 +0300

    row

--- site/about.php
+++ site/about.php
@@ -208,15 +208,21 @@
                 <h2 class="head">Our Story</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex numquam quas non dolor perspiciatis, vitae quam commodi quos officia sapiente iure similique minus cum molestiae odio earum atque corrupti cupiditate.</p>
             </div>
-        </section>
-        <section>
             <div class="container text-center">
                 <h2>Our Mission</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quae necessitatibus commodi natus fugit. Nisi neque voluptatum aut vero sed, labore impedit repellat consequuntur qui, quibusdam perspiciatis error ipsa nobis!</p>
             </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-6">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, perferendis.</p>
+                    </div>
+                    <div class="col-6">
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore voluptatum a dicta aspernatur fuga cum.</p>
+                    </div>
+                </div>
+            </div>
         </section>
-        <section></section>
-        <section></section>
         <section></section>
     </main>
 

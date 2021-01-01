807797eeda4e1a3dd55b693c04e0b6d44a240fa4
fortunate200-lang <mugishafortunate2@gmail.com>
2021-01-01 17:13:26 +0300

    signin form

--- signin.php
+++ signin.php
@@ -15,7 +15,30 @@
 </head>
 
 <body>
-   
+    <form class="form-signin" method="post" action="sign_in.php">
+        <center>
+            <img class="mb-4" src="favicon.ico" alt="" height="100">
+            <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+        </center>
+
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" name="email" required autofocus><br>
+
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" name="password" required>
+
+        <div class="checkbox mb-3">
+            <label>
+                <input type="checkbox" value="remember-me"> Remember me
+            </label>
+        </div>
+        <button class="btn btn-lg btn-warning btn-block" style="background-color: #59b8b2;" name="login_user" type="submit">Sign in</button><br>
+
+        <center>
+            <small>Don't have an a/c </small> <a href="sign_up.php" style="color:red;">Sign up Now</a>
+            <p class="mt-5 mb-3 text-muted">&copy; 2020</p>
+        </center>
+    </form>
 
     <script src="assets/scripts/jquery.min.js"></script>
     <script src="assets/js/popper.min.js"></script>

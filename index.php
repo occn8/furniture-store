0dddbd6cd9e0b1dc2c63a6128d528b864fb80e9d
tishakirabo <muhanguzitisha@gmail.com>
2021-01-05 16:03:36 +0300

    home page control

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ include('widgets/header.php');
             <h4 class="text-center"><b>Chaise longues</b></h4>
             <div class="owl-carousel owl-theme">
                 <?php
-                $result = $connect->query($querryarmchairs);
+                $result = $connect->query($querrychaiselongues);
                 while ($row = mysqli_fetch_assoc($result)) {
                     component($row['product_name'], $row['product_price'], $row['product_image'], $row['id']);
                 }
@@ -43,7 +43,7 @@ include('widgets/header.php');
             <h4 class="text-center"><b>Cushions</b></h4>
             <div class="owl-carousel owl-theme">
                 <?php
-                $result = $connect->query($querryarmchairs);
+                $result = $connect->query($querrycushions);
                 while ($row = mysqli_fetch_assoc($result)) {
                     component($row['product_name'], $row['product_price'], $row['product_image'], $row['id']);
                 }
@@ -56,7 +56,7 @@ include('widgets/header.php');
             <h4 class="text-center"><b>Daybeds</b></h4>
             <div class="owl-carousel owl-theme">
                 <?php
-                $result = $connect->query($querryarmchairs);
+                $result = $connect->query($querrydaybeds);
                 while ($row = mysqli_fetch_assoc($result)) {
                     component($row['product_name'], $row['product_price'], $row['product_image'], $row['id']);
                 }
@@ -66,10 +66,10 @@ include('widgets/header.php');
     </section>
     <section id="custom-sale">
         <div class="container py-5">
-            <h4 class="text-center"><b>Easychairs</b></h4>
+            <h4 class="text-center"><b>Easy chairs</b></h4>
             <div class="owl-carousel owl-theme">
                 <?php
-                $result = $connect->query($querryarmchairs);
+                $result = $connect->query($querryEasychairs);
                 while ($row = mysqli_fetch_assoc($result)) {
                     component($row['product_name'], $row['product_price'], $row['product_image'], $row['id']);
                 }
@@ -82,7 +82,7 @@ include('widgets/header.php');
             <h4 class="text-center"><b>Footstools</b></h4>
             <div class="owl-carousel owl-theme">
                 <?php
-                $result = $connect->query($querryarmchairs);
+                $result = $connect->query($querryFootstools);
                 while ($row = mysqli_fetch_assoc($result)) {
                     component($row['product_name'], $row['product_price'], $row['product_image'], $row['id']);
                 }
@@ -95,7 +95,7 @@ include('widgets/header.php');
             <h4 class="text-center"><b>Kidssofas</b></h4>
             <div class="owl-carousel owl-theme">
                 <?php
-                $result = $connect->query($querryarmchairs);
+                $result = $connect->query($querryKidssofas);
                 while ($row = mysqli_fetch_assoc($result)) {
                     component($row['product_name'], $row['product_price'], $row['product_image'], $row['id']);
                 }
@@ -108,7 +108,7 @@ include('widgets/header.php');
             <h4 class="text-center"><b>Poufs</b></h4>
             <div class="owl-carousel owl-theme">
                 <?php
-                $result = $connect->query($querryarmchairs);
+                $result = $connect->query($querryPoufs);
                 while ($row = mysqli_fetch_assoc($result)) {
                     component($row['product_name'], $row['product_price'], $row['product_image'], $row['id']);
                 }
@@ -121,7 +121,7 @@ include('widgets/header.php');
             <h4 class="text-center"><b>Small sofas</b></h4>
             <div class="owl-carousel owl-theme">
                 <?php
-                $result = $connect->query($querryarmchairs);
+                $result = $connect->query($querrySmallsofas);
                 while ($row = mysqli_fetch_assoc($result)) {
                     component($row['product_name'], $row['product_price'], $row['product_image'], $row['id']);
                 }
@@ -134,7 +134,7 @@ include('widgets/header.php');
             <h4 class="text-center"><b>Sofa</b></h4>
             <div class="owl-carousel owl-theme">
                 <?php
-                $result = $connect->query($querryarmchairs);
+                $result = $connect->query($querrySofa);
                 while ($row = mysqli_fetch_assoc($result)) {
                     component($row['product_name'], $row['product_price'], $row['product_image'], $row['id']);
                 }

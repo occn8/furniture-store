b4486df9132c608e9533fa4d61ec0866501a6063
klivekaru08 <klive-karu@outlook.com>
2021-01-05 10:32:49 +0300

    all products test

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ include('widgets/header.php');
             <h4 class="text-center"><b>Top Sales</b></h4>
             <div class="owl-carousel owl-theme">
                 <?php
-                $result = $connect->query($querrypdts);
+                $result = $connect->query($querryarmchairs);
                 while ($row = mysqli_fetch_assoc($result)) {
                     component($row['product_name'], $row['product_price'], $row['product_image'], $row['id']);
                 }
@@ -30,7 +30,7 @@ include('widgets/header.php');
             <h4 class="text-center"><b>Best Deals</b></h4>
             <div class="owl-carousel owl-theme">
                 <?php
-                $result = $connect->query($querrypdts);
+                $result = $connect->query($querryarmchairs);
                 while ($row = mysqli_fetch_assoc($result)) {
                     component($row['product_name'], $row['product_price'], $row['product_image'], $row['id']);
                 }
